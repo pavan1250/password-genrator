3702f1616a1db7bc937c274a1e9ae6a75ac2ca59
pavan1250 <jpavankumar39267@gmail.com>
2025-01-29 20:27:18 +0530

    Add files via upload

---
+++ passwordgenrator.js
@@ -0,0 +1,31 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const includeUppercase = document.getElementById("includeUppercase").checked;
+    const includeNumbers = document.getElementById("includeNumbers").checked;
+    const includeSymbols = document.getElementById("includeSymbols").checked;
+
+    let lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    let uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numberChars = "0123456789";
+    let symbolChars = "!@#$%^&*()_+[]{}<>?";
+
+    let allChars = lowercaseChars;
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSymbols) allChars += symbolChars;
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    let passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+}
